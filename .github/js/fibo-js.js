6aaecd61debd6a3001dbb2134f5afa72db897dcf
Avraham Kashy <avraham.kashy@gmail.com>
2022-06-19 17:31:49 +0300

    Fibonacci milestone1

---
+++ .github/js/fibo-js.js
@@ -0,0 +1,6 @@
+
+let inputPlaceHolder = document.getElementById("input");
+let outputPlaceHolder = document.getElementById("output");
+
+inputPlaceHolder.innerText = 5;
+outputPlaceHolder.innerText = 3;
