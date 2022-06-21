6da724e819c85939307ee5ebb37ed98284add153
Avraham Kashy <avraham.kashy@gmail.com>
2022-06-21 20:53:24 +0300

    Javascript Project 1 - Milestone5 solutions Avraham Kashy

--- .github/js/fibo-js.js
+++ .github/js/fibo-js.js
@@ -1,13 +1,33 @@
 let inputPlaceHolder = document.getElementById("fiboInput").value;
-
 let outputPlaceHolder = document.getElementById("fiboOutput");
+let largerPlaceHolder = document.getElementById("numLarger");
+let networkError = document.getElementById("sr-only");
 
 document.getElementById("myButton").addEventListener("click", function () {
+
+  outputPlaceHolder.innerText = ""; 
+  largerPlaceHolder.innerText = "";
+  networkError.innerText = "";
+
   tempInput = document.getElementById("fiboInput").value;
 
-  fetch(`http://localhost:5050/fibonacci/+${tempInput}`).then((response) => {
-    response.json().then((data) => {
+  if (tempInput > 50) {
+    largerPlaceHolder.innerText = "Can't be larger than 50";  
+  }
+
+  else {
+    document.getElementById("spinnerChecking").classList.remove("d-none");
+    fetch(`http://localhost:5050/fibonacci/+${tempInput}`).then((response) => {
+    if (!response.ok) {
+      document.getElementById("spinnerChecking").classList.add("d-none");
+      networkError.innerText = "Server error: 42 is the meaning of life";
+      return;
+    }else {
+      response.json().then((data) => {
+      document.getElementById("spinnerChecking").classList.add("d-none");
       outputPlaceHolder.innerText = data.result;
     });
+  }
   });
+}
 });
